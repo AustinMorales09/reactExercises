374d634b79452a1e10ad37ff64cd4f1bd5452bda
Austin <Harrenaustint@gmail.com>
2022-05-04 13:36:10 -0400

    pushing 21-26 finished

--- index.js
+++ index.js
@@ -601,9 +601,9 @@ Camper.propTypes = { name: PropTypes.string.isRequired};
 
 // You create state in a React component by declaring a state property on the component class in its constructor. This initializes the component with state when it is created. The state property must be set to a JavaScript object. Declaring it looks like this:
 
-// this.state = {
+this.state = {
 
-// }
+}
 // You have access to the state object throughout the life of your component. You can update it, render it in your UI, and pass it as props to child components. The state object can be as complex or as simple as you need it to be. Note that you must create a class component by extending React.Component in order to create state like this.
 
 // There is a component in the code editor that is trying to render a name property from its state. However, there is no state defined. Initialize the component with state in the constructor and assign your name to a property of name.
@@ -611,7 +611,9 @@ class StatefulComponent extends React.Component {
   constructor(props) {
     super(props);
     // Only change code below this line
-
+      this.state = {
+        name: 'Austin'
+      }
     // Only change code above this line
   }
   render() {
@@ -647,7 +649,7 @@ class MyComponent extends React.Component {
     return (
       <div>
         { /* Change code below this line */ }
-
+        <h1>{this.state.name}</h1>
         { /* Change code above this line */ }
       </div>
     );
@@ -670,12 +672,12 @@ class MyComponent extends React.Component {
   }
   render() {
     // Change code below this line
-
+      const name = this.state.name;
     // Change code above this line
     return (
       <div>
         { /* Change code below this line */ }
-
+        {name}
         { /* Change code above this line */ }
       </div>
     );
@@ -685,9 +687,9 @@ class MyComponent extends React.Component {
 // // 24 Set State with this.setState
 // The previous challenges covered component state and how to initialize state in the constructor. There is also a way to change the component's state. React provides a method for updating component state called setState. You call the setState method within your component class like so: this.setState(), passing in an object with key-value pairs. The keys are your state properties and the values are the updated state data. For instance, if we were storing a username in state and wanted to update it, it would look like this:
 
-// this.setState({
-//   username: 'Lewis'
-// });
+this.setState({
+  username: 'Lewis'
+});
 // React expects you to never modify state directly, instead always use this.setState() when state changes occur. Also, you should note that React may batch multiple state updates in order to improve performance. What this means is that state updates through the setState method can be asynchronous. There is an alternative syntax for the setState method which provides a way around this problem. This is rarely needed but it's good to keep it in mind! Please consult the React documentation for further details.
 
 // There is a button element in the code editor which has an onClick() handler. This handler is triggered when the button receives a click event in the browser, and runs the handleClick method defined on MyComponent. Within the handleClick method, update the component state using this.setState(). Set the name property in state to equal the string React Rocks!.
@@ -703,7 +705,9 @@ class MyComponent extends React.Component {
   }
   handleClick() {
     // Change code below this line
-
+      this.setState({
+        name: 'React Rocks!'
+      })
     // Change code above this line
   }
   render() {
@@ -721,8 +725,8 @@ class MyComponent extends React.Component {
 // 25 Bind 'this' to a Class Method 
 // In addition to setting and updating state, you can also define methods for your component class. A class method typically needs to use the this keyword so it can access properties on the class (such as state and props) inside the scope of the method. There are a few ways to allow your class methods to access this.
 
-// One common way is to explicitly bind this in the constructor so this becomes bound to the class methods when the component is initialized. You may have noticed the last challenge used this.handleClick = this.handleClick.bind(this) for its handleClick method in the constructor. Then, when you call a function like this.setState() within your class method, this refers to the class and will not be undefined.
-
+// One common way is to explicitly bind this in the constructor so this becomes bound to the class methods when the component is initialized. You may have noticed the last challenge used  for its handleClick method in the constructor. Then, when you call a function like this.setState() within your class method, this refers to the class and will not be undefined.
+this.handleClick = this.handleClick.bind(this)
 // Note: The this keyword is one of the most confusing aspects of JavaScript but it plays an important role in React. Although its behavior here is totally normal, these lessons aren't the place for an in-depth review of this so please refer to other lessons if the above is confusing!
 
 // The code editor has a component with a state that keeps track of the text. It also has a method which allows you to set the text to You clicked!. However, the method doesn't work because it's using the this keyword that is undefined. Fix it by explicitly binding this to the handleClick() method in the component's constructor.
@@ -738,7 +742,7 @@ class MyComponent extends React.Component {
       text: "Hello"
     };
     // Change code below this line
-
+    this.handleClick = this.handleClick.bind(this);
     // Change code above this line
   }
   handleClick() {
@@ -750,7 +754,7 @@ class MyComponent extends React.Component {
     return (
       <div>
         { /* Change code below this line */ }
-        <button>Click Me</button>
+        <button onClick={this.handleClick}>Click Me</button>
         { /* Change code above this line */ }
         <h1>{this.state.text}</h1>
       </div>
@@ -762,19 +766,19 @@ class MyComponent extends React.Component {
 // // 26 Use State to Toggle an Element
 // Sometimes you might need to know the previous state when updating the state. However, state updates may be asynchronous - this means React may batch multiple setState() calls into a single update. This means you can't rely on the previous value of this.state or this.props when calculating the next value. So, you should not use code like this:
 
-// this.setState({
-//   counter: this.state.counter + this.props.increment
-// });
+this.setState({
+  counter: this.state.counter + this.props.increment
+});
 // Instead, you should pass setState a function that allows you to access state and props. Using a function with setState guarantees you are working with the most current values of state and props. This means that the above should be rewritten as:
 
-// this.setState((state, props) => ({
-//   counter: state.counter + props.increment
-// }));
+this.setState((state, props) => ({
+  counter: state.counter + props.increment
+}));
 // You can also use a form without props if you need only the state:
 
-// this.setState(state => ({
-//   counter: state.counter + 1
-// }));
+this.setState(state => ({
+  counter: state.counter + 1
+}));
 // Note that you have to wrap the object literal in parentheses, otherwise JavaScript thinks it's a block of code.
 
 // MyComponent has a visibility property which is initialized to false. The render method returns one view if the value of visibility is true, and a different view if it is false.
@@ -792,11 +796,19 @@ class MyComponent extends React.Component {
       visibility: false
     };
     // Change code below this line
-
+    this.toggleVisibility = this.toggleVisibility.bind(this);
     // Change code above this line
   }
   // Change code below this line
-
+  toggleVisibility(){
+    this.setState(state =>{
+      if(state.visibility === true){
+        return { visibility: false};
+      } else{
+        return { visibility: true};
+      }
+    })
+  }
   // Change code above this line
   render() {
     if (this.state.visibility) {
