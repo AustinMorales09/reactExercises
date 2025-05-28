fbfc5cccb4fab67bbf20bce7b598676396f0a25b
your name <harrenaustint@gmail.com>
2025-05-28 09:08:30 -0400

    pushing 21-35

---
+++ test.js
@@ -0,0 +1,28 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// Change code below this line
+const Camper = (props) =>{
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  )
+}
+
+Camper.defaultProps = {
+  name: 'CamperBot'
+}
+
+Camper.propTypes = {name: PropTypes.string.isRequired}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CampSite />);
